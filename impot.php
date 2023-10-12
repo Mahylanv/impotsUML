f7851e131b7a01e19e89a37108b26ba87a32c89a
Mahylan <mahylan.veclin@wangarden.com>
2023-10-12 12:13:21 +0200

    modification en constante des pourcentages

--- impot.php
+++ impot.php
@@ -1,21 +1,19 @@
 <?php
 class ImpotCalculator {
     private $revenus;
+    const POURCENTAGE_FAIBLE = 15;
+    const POURCENTAGE_ELEVE = 20;
 
     public function __construct($revenus) {
         $this->revenus = $revenus;
     }
 
     public function calculerImpot() {
-        if ($this->revenus < 15000) {
-            $pourcentage = 15;
-        } else {
-            $pourcentage = 20;
-        }
+        $pourcentage = ($this->revenus < 15000) ? self::POURCENTAGE_FAIBLE : self::POURCENTAGE_ELEVE;
 
-        $montantImpot = ($pourcentage / 100) * $this->revenus;
+        $impot = ($pourcentage / 100) * $this->revenus;
 
-        return $montantImpot;
+        return $impot;
     }
 }
 
